3c3759dbaefdfade314cb305d524da48e8a7a1d0
C0dingChris <Chris.Sanger@outlook.com>
2022-11-29 13:35:48 +0000

    Optimised code, deleted some unnesecary divs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,46 +5,48 @@ describe("visit site", () => {
 });
 
 describe("test general user flow of the home page", () => {
-  it("the div clicked", () => {
+  it("The angry duck container selected", () => {
     cy.get(".angry-duck-container");
   });
 
-  it("the button clicked", () => {
+  it("anon button clicked", () => {
     cy.get(".content-wrapper > div").click().wait(800);
   });
   it("the duck image clicked to go to the home", () => {
     cy.get(".duck-img").click().wait(800);
   });
-  it("the button clicked again", () => {
+  it("anon button clicked again", () => {
     cy.get(".content-wrapper > div").click().wait(800);
   });
 });
 
-describe("test general user flow of the home page", () => {
-  it("the title exist", () => {
+describe("test general user flow of the forum page", () => {
+  it("check H1 is rendered", () => {
     cy.get(".read").wait(800);
   });
-  it("the title input box working", () => {
+  it("Clicked on the title input box and typed", () => {
     cy.get(".title-div > textarea")
       .click()
       .type("my new post from cypress")
       .wait(800);
   });
-  it("the content input box working", () => {
+  it("Clicked on the content input box and typed", () => {
     cy.get(".content-div > textarea")
       .click()
       .type("my new post from cypress")
       .wait(800);
   });
-  // it("the submit post button is working", () => {
-  //   cy.get(".button")
-  //     .click()
-  // .wait(800);
-  // });
-  it("posts are open up when clicked", () => {
+  it("Clicked on the submit post button", () => {
+    cy.get(".button")
+      .click()
+  .wait(800);
+  });
+  it("Checks first post expands upon click", () => {
     cy.get(":nth-child(1) > .post > .post-title-btn").click().wait(800);
   });
 });
+
+//Doesn't currently work but will rework 
 // it("the comment input box working", () => {
 //   cy.get('label > textarea')
 // .click()
